181e34e6cfd6006b0c83308e3074fb8813c3192b
durgeshrnw <durgeshikari20@gmail.com>
2024-08-23 15:37:59 +0530

    update

---
+++ tringle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define angle 180
+void main()
+{
+    float a,b,c;
+    printf("Enter the A: ");
+    scanf("%f",&a);
+    printf("Entre the B: ");
+    scanf("%f",&b);
+    printf("Enter the C: ");
+    scanf("%f",&c);
+    printf("area of c=%.2f\n",a+b+c);
+}    
